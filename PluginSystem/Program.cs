09b18f22b6afacbd619544c78c787fbcd9639b3d
Belogrivtsev <nuclearthrone55@gmail.com>
2025-07-05 00:52:28 +0600

    feat: plugins system and test plugin for it

---
+++ PluginSystem/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+namespace PluginSystem;
+public class PluginLoadAttribute : Attribute
+{
+    public string[] Dependencies { get; }
+    public PluginLoadAttribute(params string[] dependencies) { Dependencies = dependencies; }
+}
+public class PluginSystem
+{
+    public static void Run()
+    {
+        var plugins = Directory.EnumerateFiles("Plugins", "*.dll").Select(Assembly.LoadFrom).SelectMany(t => t.GetExportedTypes())
+        .Where(t => t.GetCustomAttribute<PluginLoadAttribute>() != null).ToList();
+
+        if (!plugins.Any())
+        {
+            Console.WriteLine("Plugins not found");
+            return;
+        }
+
+        plugins.ForEach(t =>
+        {
+            var instance = Activator.CreateInstance(t);
+            var execute = t.GetMethod("Execute");
+            execute?.Invoke(instance, null);
+        });
+    }
+}
+public class App
+{
+    public static void Main(string[] args)
+    {
+        PluginSystem.Run();
+    }
+}
---
+++ TestPlugin/Class1.cs
@@ -0,0 +1,8 @@
+﻿using PluginSystem;
+namespace TestPlugin;
+
+[PluginLoad]
+public class TestPlugin
+{
+    public void Execute() { Console.WriteLine("This plugin is used"); }
+}
